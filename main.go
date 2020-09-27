195395f5f1f6ff010821081d8bc7864c34451ff4
Mitchell Katz (Sk1er) <sk1er@sk1er.c.ub>
2020-09-27 16:40:25 -0400

    Update: Reference to MerkleTree to use path relative to the main.go file

--- main.go
+++ main.go
@@ -1,118 +1,118 @@
 package main
 
 import (
-  "CSE297/BlockchainProject/MerkleTree"
-  "bufio"
-  "fmt"
-  "log"
-  "os"
-  "sort"
+	"./MerkleTree"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
 )
 
 func readFile(filename string) ([]string, error) {
-  // Open and read file
-  file, err := os.Open(filename)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer file.Close()
+	// Open and read file
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-  scanner := bufio.NewScanner(file)
-  var lines []string
-  for scanner.Scan() {
-    lines = append(lines, scanner.Text())
-    if len(scanner.Text()) > 100 {
-      log.Fatal("String input to large")
-    }
-  }
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+		if len(scanner.Text()) > 100 {
+			log.Fatal("String input to large")
+		}
+	}
 
-  return lines, scanner.Err()
+	return lines, scanner.Err()
 }
 
 func writeArray(tree []string, file *os.File) bool {
-  // Write to the file
-  _, err := file.WriteString("Starting to print the tree\n")
-  // Make sure write was successful
-  if err != nil {
-    log.Fatal(err)
-  }
-  // Begin printing out the contents of the array
-  for i := 0; i < len(tree); i++ {
-    _, err2 := file.WriteString(tree[i] + "\n")
-    // Make sure write was successful
-    if err2 != nil {
-      log.Fatal(err2)
-    }
-  }
-  return false
+	// Write to the file
+	_, err := file.WriteString("Starting to print the tree\n")
+	// Make sure write was successful
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Begin printing out the contents of the array
+	for i := 0; i < len(tree); i++ {
+		_, err2 := file.WriteString(tree[i] + "\n")
+		// Make sure write was successful
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+	}
+	return false
 }
 
 func writeTree(root *MerkleTree.TreeNode, file *os.File) bool {
-  // Write to the file
-  _, err2 := file.WriteString("Hello GoLang")
-  // Make sure write was successful
-  if err2 != nil {
-    log.Fatal(err2)
-  }
+	// Write to the file
+	_, err2 := file.WriteString("Hello GoLang")
+	// Make sure write was successful
+	if err2 != nil {
+		log.Fatal(err2)
+	}
 
-  // Return
-  return false
+	// Return
+	return false
 }
 
 func print(root interface{}) {
-  switch root.(type) {
-    case *MerkleTree.LeafNode:
-      fmt.Println("Leaf: ", root.(*MerkleTree.LeafNode).Key)
-    case *MerkleTree.TreeNode:
-      fmt.Println("TreeHash: ", root.(*MerkleTree.TreeNode).Hash)
-      fmt.Println("TreeLeft: ", root.(*MerkleTree.TreeNode).LeftEdge)
-      fmt.Println("TreeRight: ", root.(*MerkleTree.TreeNode).RightEdge)
-      fmt.Println()
-      print(root.(*MerkleTree.TreeNode).Left)
-      print(root.(*MerkleTree.TreeNode).Right)
-  }
+	switch root.(type) {
+	case *MerkleTree.LeafNode:
+		fmt.Println("Leaf: ", root.(*MerkleTree.LeafNode).Key)
+	case *MerkleTree.TreeNode:
+		fmt.Println("TreeHash: ", root.(*MerkleTree.TreeNode).Hash)
+		fmt.Println("TreeLeft: ", root.(*MerkleTree.TreeNode).LeftEdge)
+		fmt.Println("TreeRight: ", root.(*MerkleTree.TreeNode).RightEdge)
+		fmt.Println()
+		print(root.(*MerkleTree.TreeNode).Left)
+		print(root.(*MerkleTree.TreeNode).Right)
+	}
 }
 
 func main() {
-  fmt.Print("Enter the filename: ")
-  var filename string
-  fmt.Scanln(&filename)
-  fmt.Println("Filename is: " + filename)
+	fmt.Print("Enter the filename: ")
+	var filename string
+	fmt.Scanln(&filename)
+	fmt.Println("Filename is: " + filename)
 
-  lines, err := readFile(filename)
-  if err != nil {
-    log.Fatal(err)
-  }
-  sort.Strings(lines)
+	lines, err := readFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sort.Strings(lines)
 
-  // Create array of constructed lead nodes
-  leafs := make([]*MerkleTree.LeafNode, len(lines))
-  for i, line := range lines {
-    leafs[i] = MerkleTree.CreateLeafNode(line)
-  }
-  trie := MerkleTree.CreateTrie()
-  MerkleTree.Construct(leafs, trie)
-  fmt.Println("Root left edge: ", trie.Root.LeftEdge)
-  fmt.Println("Root right edge: ", trie.Root.RightEdge)
-  fmt.Println("Left leftedge: ", trie.Root.Left.(MerkleTree.TreeNode).LeftEdge)
-  fmt.Println("Left rightedge: ", trie.Root.Left.(MerkleTree.TreeNode).RightEdge)
+	// Create array of constructed lead nodes
+	leafs := make([]*MerkleTree.LeafNode, len(lines))
+	for i, line := range lines {
+		leafs[i] = MerkleTree.CreateLeafNode(line)
+	}
+	trie := MerkleTree.CreateTrie()
+	MerkleTree.Construct(leafs, trie)
+	fmt.Println("Root left edge: ", trie.Root.LeftEdge)
+	fmt.Println("Root right edge: ", trie.Root.RightEdge)
+	fmt.Println("Left leftedge: ", trie.Root.Left.(MerkleTree.TreeNode).LeftEdge)
+	fmt.Println("Left rightedge: ", trie.Root.Left.(MerkleTree.TreeNode).RightEdge)
 
-  //print(trie.Root)
+	//print(trie.Root)
 
-  //// split the file name to adhere to output format
-  //splitFile := strings.Split(filename, ".")
-  //outFile := splitFile[0] + ".out.txt"
-  //// Create the file to write to
-  //file, err := os.Create(outFile)
-  //// Make sure creation completed
-  //if err != nil {
-  //  log.Fatal(err)
-  //}
-  //// Set the file to close when finished
-  //defer file.Close()
-  //
-  //array := []string{"first", "second", "third", "fourth", "fifth"}
-  //printed := writeArray(array, file)
-  //
-  //fmt.Println("Success? ", printed)
+	//// split the file name to adhere to output format
+	//splitFile := strings.Split(filename, ".")
+	//outFile := splitFile[0] + ".out.txt"
+	//// Create the file to write to
+	//file, err := os.Create(outFile)
+	//// Make sure creation completed
+	//if err != nil {
+	//  log.Fatal(err)
+	//}
+	//// Set the file to close when finished
+	//defer file.Close()
+	//
+	//array := []string{"first", "second", "third", "fourth", "fifth"}
+	//printed := writeArray(array, file)
+	//
+	//fmt.Println("Success? ", printed)
 }
